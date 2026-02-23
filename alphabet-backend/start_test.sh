477709fe35cd0b9bf92b6498780023ed87205afb
Tapeline <mail@tapeline.dev>
2026-02-23 05:25:17 +0500

    Package dummy iap and try to measure coverage

---
+++ alphabet-backend/start_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+uv run alembic upgrade head
+
+# This is some smartypants move suggested by Gemini
+# Basically, we want coverage to finish writing, so
+# we need to gracefully shutdown.
+
+uv run coverage run -p -m alphabet.bootstrap.entrypoints.cli run server &
+PID_UVICORN=$!
+uv run coverage run -p -m alphabet.bootstrap.entrypoints.cli run attribution_worker &
+PID_ATTR=$!
+uv run coverage run -p -m alphabet.bootstrap.entrypoints.cli run guardrails_worker &
+PID_GUARD=$!
+uv run coverage run -p -m alphabet.bootstrap.entrypoints.cli run notifications_worker &
+PID_NOTIF=$!
+
+# Here goes some linux magic I don't understand
+
+trap "kill -SIGTERM $PID_UVICORN $PID_ATTR $PID_GUARD $PID_NOTIF" SIGTERM SIGINT
+wait $PID_UVICORN $PID_ATTR $PID_GUARD $PID_NOTIF
